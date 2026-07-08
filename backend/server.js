46832e842330278dd7a9f7f704be90c024dcddc9
Beyza <apple@MacBook-Pro.local>
2026-07-08 10:32:56 +0300

    Complete backend Express API

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully!");
+});
+
+app.get("/hello", (req, res) => {
+  const name = req.query.name;
+
+  res.json({
+    message: `Hello ${name}!`
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
